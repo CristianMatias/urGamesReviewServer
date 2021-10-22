2752c45b1d2ee94000ac32f988eea82ad9538244
CristianMatias <cristianmatias52@gmail.com>
2021-10-22 10:55:56 +0200

    Initial commit

---
+++ src/main/java/com/gamesreview/server/UrGamesReviewApplication.java
@@ -0,0 +1,13 @@
+package com.gamesreview.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrGamesReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrGamesReviewApplication.class, args);
+	}
+
+}
